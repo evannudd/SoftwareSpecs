d08a1ebd20718e1387b44241f71dca5ca2df6e71
owen <helloimowen>
2017-09-12 15:04:13 -0400

    my first push :^)

--- AirlineProject/src/Customer.java
+++ AirlineProject/src/Customer.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
 public class Customer {
+	private ArrayList<Reservation> myReservations = new ArrayList<Reservation>();
 
+	public ArrayList<Reservation> getMyReservation() {
+		return myReservations;
+	}
+
+	public void setMyReservation(ArrayList<Reservation> myReservation) {
+		this.myReservations = myReservation;
+	}
+
+	@Override
+	public String toString() {
+		String itinerary = "";
+		
+		for (int i = 0; i < myReservations.size(); i++) {
+			
+			itinerary += myReservations.get(i).toString(); 
+			
+		}
+		return "Here is your itinerary: [myReservation=" + myReservations + "]";
+	} 
 }
