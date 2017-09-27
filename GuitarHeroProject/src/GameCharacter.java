64482fb660dfb71767e24d02564e635d065fa5e3
evannudd <evan.nudd@mymail.champlain.edu>
2017-09-27 13:57:46 -0400

    added files, with comments

--- GuitarHeroProject/src/GameCharacter.java
+++ GuitarHeroProject/src/GameCharacter.java
@@ -1,5 +1,24 @@
+/*
+ * Author: Evan Nudd
+ * Class: CSI-340-01
+ * Assignment: Lab - 02 Guitar Hero Project
+ * Due Date: Thursday September 27 2pm
+ *
+ * Description:
+ *
+ * Certification of Authenticity: I  certify  that  this  is  entirely
+ *  my  own  work,  except  where  I  have  given fully-documented
+ *  references  to  the  work  of  others.  I  understand  the definition
+ *  and consequences of plagiarism and acknowledge that the assessor of
+ *  this assignment may, for the purpose of assessing this assignment:-
+ *  Reproduce  this  assignment  and  provide  a  copy  to  another
+ *  member  of academic staff; and/or-Communicate  a  copy  of  this
+ *  assignment  to  a  plagiarism  checking service  (which  may  then
+ *  retain  a  copy  of  this  assignment  on  its database for the purpose of
+ *  future plagiarism checking)
+ */
 
-public class GameCharacter {
+public abstract class GameCharacter {
 
 	private String name;
 
@@ -8,11 +27,11 @@ public class GameCharacter {
 	private Guitar guitar;
 
 	public void playGuitar() {
-
+			guitar.playGuitar();
 	}
 
 	public void playSolo() {
-
+			soloBehavior.playSolo();
 	}
 
 	public String getName() {
--- GuitarHeroProject/src/GameCharacterSlash.java
+++ GuitarHeroProject/src/GameCharacterSlash.java
@@ -1,4 +1,23 @@
-
+/*
+ * Author: Evan Nudd
+ * Class: CSI-340-01
+ * Assignment: Lab - 02 Guitar Hero Project
+ * Due Date: Thursday September 27 2pm
+ *
+ * Description:
+ *
+ * Certification of Authenticity: I  certify  that  this  is  entirely
+ *  my  own  work,  except  where  I  have  given fully-documented
+ *  references  to  the  work  of  others.  I  understand  the definition
+ *  and consequences of plagiarism and acknowledge that the assessor of
+ *  this assignment may, for the purpose of assessing this assignment:-
+ *  Reproduce  this  assignment  and  provide  a  copy  to  another
+ *  member  of academic staff; and/or-Communicate  a  copy  of  this
+ *  assignment  to  a  plagiarism  checking service  (which  may  then
+ *  retain  a  copy  of  this  assignment  on  its database for the purpose of
+ *  future plagiarism checking)
+ */
+ 
 public class GameCharacterSlash extends GameCharacter {
 	public GameCharacterSlash() {
 		setName("Slash");
